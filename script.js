6012a15f482736b78161f9373e3459a8052ecbe8
gazgiz <jlynnmail@gmail.com>
2015-06-11 18:06:49 -0500

    Adventure Begin
    We begin our journey.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+beenToBog = true;
+
+bug = false;
+
+currentArea = "nBog"
+
+$(document).ready(function(){
+
+    $("#console").fadeIn(2000);
+
+    $("form").submit(function(){
+      var input = $("#cmdLine").val();
+
+      if(input == "help"){
+          $("#msgHelp").clone().insertBefore("#placeholder").fadeIn(1000);
+      }
+      else if(input == "take bug" && currentArea = "nBog"){
+          $("<p>The bug is in your hands.</p>").insertBefore("#placeholder").fadeIn(1000);
+      }
+      $("#cmdLine").val("");
+
+    })
+
+});
