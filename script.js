3d940b36941fb2e682ee788052848be62cb0c150
gazgiz <jlynnmail@gmail.com>
2015-06-11 19:05:00 -0500

    Added commands
    Many a condition

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-
-beenToBog = true;
-
 bug = false;
 
 currentArea = "nBog"
 
 $(document).ready(function(){
 
-    $("#console").fadeIn(2000);
-
     $("form").submit(function(){
       var input = $("#cmdLine").val();
 
       if(input == "help"){
-          $("#msgHelp").clone().insertBefore("#placeholder").fadeIn(1000);
+          $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+          $("#msgHelp").clone().insertBefore("#placeholder").fadeIn(0)
       }
-      else if(input == "take bug" && currentArea = "nBog"){
-          $("<p>The bug is in your hands.</p>").insertBefore("#placeholder").fadeIn(1000);
+      else if(input == "take bug" && currentArea == "nBog" && bug == false){
+          $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+          $("<p>The bug is in your hands.</p>").insertBefore("#placeholder")
+          bug = true
       }
+      else if(input == "take bug" && currentArea == "nBog" && bug == true){
+          $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+          $("<p>You already have the bug.</p>").insertBefore("#placeholder")
+      }
+      else if(input =="take bug" && currentArea != "nBog"){
+          $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+          $("<p>You don't see a bug here.</p>").insertBefore("#placeholder")
+      }
+      else if(input =="go west" && currentArea == "nBog"){
+          currentArea = "wBog"
+          $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+          $("<p>You are now in the west bog.</p>").insertBefore("#placeholder")
+      }
+      else if(input =="go west" && currentArea != "nBog"){
+          $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+          $("<p>You cannot travel west from here.</p>").insertBefore("#placeholder")
+      }
+      else{
+          $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+          $("<p>I don't understand "+input+".</p>").insertBefore("#placeholder")
+      }
+
       $("#cmdLine").val("");
 
     })
