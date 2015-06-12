f65a6c1e4f83c6b686c0028da644d64c3945131a
gazgiz <jlynnmail@gmail.com>
2015-06-12 01:09:49 -0500

    generalized combat function

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
+inventory = [10];
+
 bug = false;
 hitpoints = 100;
 
 currentArea = "nBog"
 
-function comBat(){
-      var bugHP = Math.random()*100;
-      $("<p>You attack the bug!</p>").insertBefore("#placeholder")
 
+function comBat(hp, enemy){
+
+      $("<p>You attack the"+enemy+"!</p>").insertBefore("#placeholder")
+      var enemyHP = hp
       var combat =  setInterval(function(){
-                    if(bugHP > 0 && hitpoints > 0){
                       var pdmg = Math.floor(Math.random()*20+1);
-                      var edmg = Math.floor(Math.random()*20+1);
-                      bugHP -= pdmg
+                      var edmg = Math.floor(Math.random()*15+1);
+                      enemyHP -= pdmg
                       hitpoints -= edmg
-                      $("<p>You strike the bug for "+pdmg+" damage.</p>").insertBefore("#placeholder")
+                      $("<p>You strike the "+enemy+" for "+pdmg+" damage.</p>").insertBefore("#placeholder")
                       $("#console").scrollTop($("#console")[0].scrollHeight)
-
-                      setTimeout(function(){
+                      if(hitpoints <= 0){
                           $("<p>The bug bites your back for "+edmg+"!</p>").insertBefore("#placeholder")
+                          $("<p>YOU DIED</p>").insertBefore("#placeholder")
                           $("#console").scrollTop($("#console")[0].scrollHeight)
-                      },1000)
-
-                    }else if(hitpoints <= 0){
-                        $("<p>YOU DIED</p>").insertBefore("#placeholder")
-                        $("#console").scrollTop($("#console")[0].scrollHeight)
+                          clearInterval(combat)
+                      }else if(enemyHP <= 0){
+                          $("<p>The "+enemy+" has been defeated. It is dead on the ground.</p>").insertBefore("#placeholder")
+                          $("#console").scrollTop($("#console")[0].scrollHeight)
+                          bug = true;
                         clearInterval(combat)
-                    }else if(bugHP <= 0){
-                      $("<p>The bug has been defeated.</p>").insertBefore("#placeholder")
-                      $("#console").scrollTop($("#console")[0].scrollHeight)
-                      clearInterval(combat)
-                    }
-
+                      }else if(enemyHP > 0){
+                          setTimeout(function(){
+                              $("<p>The "+enemy+" bites your back for "+edmg+"!</p>").insertBefore("#placeholder")
+                              $("#console").scrollTop($("#console")[0].scrollHeight)
+                          },1000)
+                      }
                   }, 2000)
 }
 
@@ -50,19 +52,37 @@ $(document).ready(function(){
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
             $("#msgHelp").clone().insertBefore("#placeholder").fadeIn(0)
         }
+        ///////////////////northern bog bug conditions
         else if(input == "take bug" && currentArea == "nBog" && bug == false){
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-            $("<p>The bug is in your hands.</p>").insertBefore("#placeholder")
-            bug = true
+            $("<p>The bug bit you for 5 damage!</p>").insertBefore("#placeholder")
+            hitpoints -= 5
         }
         else if(input == "take bug" && currentArea == "nBog" && bug == true){
-            $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-            $("<p>You already have the bug.</p>").insertBefore("#placeholder")
+
+            if(inventory.indexOf("bug") > -1){
+              $("<p>You are already carrying the dead bug in your bag.</p>").insertBefore("#placeholder")
+            }else{
+              $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+              $("<p>You take the dead bug from the bog and place it in your bag.</p>").insertBefore("#placeholder")
+              inventory.push("bug")
+          }
         }
         else if(input =="take bug" && currentArea != "nBog"){
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
             $("<p>You don't see a bug here.</p>").insertBefore("#placeholder")
         }
+        else if(input =="attack bug" && bug == false){
+            var bugHP = Math.random()*100;
+            var enemy = "bug"
+            $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+            comBat(bugHP, enemy)
+        }
+        else if(input =="attack bug" && bug == true){
+            $("<p> >> "+input+"</p>").insertBefore("#placeholder")
+            $("<p>The bug is dead.</p>").insertBefore("#placeholder")
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////////
         else if(input =="go west" && currentArea == "nBog"){
             currentArea = "wBog"
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
@@ -71,10 +91,9 @@ $(document).ready(function(){
         else if(input =="go west" && currentArea != "nBog"){
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
             $("<p>You cannot travel west from here.</p>").insertBefore("#placeholder")
-        }
-        else if(input =="attack bug"){
-            $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-            comBat()
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        }else if(hitpoints <= 0){
+            $("<p>YOU DEAD</p>").insertBefore("#placeholder")
         }
         else{
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
