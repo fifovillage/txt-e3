a1d241ee378f443b4ae216ad565564efbe796db1
gazgiz <jlynnmail@gmail.com>
2015-06-12 22:56:14 -0500

    random encounter and bogEnemy
    removed attack bug

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-hitpoints = 100
-mana = 20
-level = 1
+player = {
+       level: 1,
+       hitpoints:100,
+       mana:20,
+       experience:0
+}
 
-inventory = [10];
+reqXP = [
+       120,
+       290,
+       445,
+       690,
+       1010,
+       1450,
+       1970,
+       2450,
+       2800,
+       3500
+]
+
+inventory = [];
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-bug = false;
+
+bogEnemy = [
+        {name:"bug", health:75, exp:15},
+        {name:"snake", health:100, exp:20},
+        {name:"crab", health:150, exp:30}]
+
 
 currentArea = "nBog"
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function refreshStatWindow(){
   $("#lvl").empty()
@@ -17,41 +40,41 @@ function refreshStatWindow(){
   $("#mp").empty()
   $("#xp").empty()
 
-  if(hitpoints < 0){
+  if(player.hitpoints < 0){
       $("#hp").append(0)
   }else{
-      $("#lvl").append("Level " + level)
-      $("#hp").append("Health: "+hitpoints)
-      $("#mp").append("Mana: "+mana)
-      $("#xp").append("Exp: ")
+      $("#lvl").append("Level " +player.level)
+      $("#hp").append("Health: "+player.hitpoints)
+      $("#mp").append("Mana: "+player.mana)
+      $("#xp").append("Exp: "+player.experience+" / "+reqXP[player.level - 1])
   }
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function comBat(hp, enemy){
+function comBat(hp, enemy, exp){
 
       $("<p>You attack the "+enemy+"!</p>").insertBefore("#Bplaceholder")
       var enemyHP = hp
       var combat =  setInterval(function(){
                       var pdmg = Math.floor(Math.random()*20+1);
                       var edmg = Math.floor(Math.random()*15+1);
-                      if(hitpoints <= 0){
+                      if(player.hitpoints <= 0){
                           $("<p>YOU DIED</p>").insertBefore("#placeholder")
                           $("#mainConsole").scrollTop($("#mainConsole")[0].scrollHeight)
                           clearInterval(combat)
                       }else if(enemyHP <= 0){
                           $("<p>The "+enemy+" has been defeated. It is dead on the ground.</p>").insertBefore("#Bplaceholder")
                           $("#combatContainer").scrollTop($("#combatContainer")[0].scrollHeight)
-                          bug = true;
+                          player.experience += exp
                         clearInterval(combat)
                       }else if(enemyHP > 0){
                           enemyHP -= pdmg
                           $("<p>You strike the "+enemy+" for "+pdmg+" damage.</p>").insertBefore("#Bplaceholder")
                           $("#combatContainer").scrollTop($("#combatContainer")[0].scrollHeight)
                           setTimeout(function(){
-                              hitpoints -= edmg
+                              player.hitpoints -= edmg
                               $("<p>The "+enemy+" bites your back for "+edmg+"!</p>").insertBefore("#Bplaceholder")
                               $("#combatContainer").scrollTop($("#combatContainer")[0].scrollHeight)
                           },1000)
@@ -64,12 +87,11 @@ function comBat(hp, enemy){
 $(document).ready(function(){
 setInterval(refreshStatWindow, 100)
 
-
     $("form").submit(function(){
       var input = $("#cmdLine").val().toLowerCase();
 
 
-      if(hitpoints > 0){
+      if(player.hitpoints > 0){
 
         if(input == "help"){
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
@@ -78,49 +100,32 @@ setInterval(refreshStatWindow, 100)
         ///////////////////northern bog bug conditions
         else if(input == "take bug" && currentArea == "nBog" && bug == false){
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-            if(hitpoints <= 5){
+            if(player.hitpoints <= 5){
               $("<p>One more time and that bug will kill you.</p>").insertBefore("#placeholder")
 
             }else{
               $("<p>The bug bit you for 5 damage!</p>").insertBefore("#placeholder")
-              hitpoints -= 5
+              player.hitpoints -= 5
             }
-        }
-        else if(input == "take bug" && currentArea == "nBog" && bug == true){
-
-            if(inventory.indexOf("bug") > -1){
-              $("<p>You are already carrying the dead bug in your bag.</p>").insertBefore("#placeholder")
-            }else{
-              $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-              $("<p>You take the dead bug from the bog and place it in your bag.</p>").insertBefore("#placeholder")
-              inventory.push("bug")
-          }
-        }
-        else if(input =="take bug" && currentArea != "nBog"){
-            $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-            $("<p>You don't see a bug here.</p>").insertBefore("#placeholder")
-        }
-        else if(input =="attack bug" && bug == false){
-            var bugHP = Math.random()*100;
-            var enemy = "bug"
-            $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-            comBat(bugHP, enemy)
-        }
-        else if(input =="attack bug" && bug == true){
-            $("<p> >> "+input+"</p>").insertBefore("#placeholder")
-            $("<p>The bug is dead.</p>").insertBefore("#placeholder")
         }
         //////////////////////////////////////////////////////////////////////////////////////////////
         else if(input =="go west" && currentArea == "nBog"){
+            var randEnc = (Math.random() * 10 + 2)
             currentArea = "wBog"
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
             $("<p>You are now in the west bog.</p>").insertBefore("#placeholder")
+
+              if(randEnc > 5){
+                var rand = Math.floor(Math.random()* 3)
+                $("<p>An aggressive "+bogEnemy[rand].name+" attacks!</p>").insertBefore("#placeholder")
+                comBat(bogEnemy[rand].health, bogEnemy[rand].name, bogEnemy[rand].exp)
+              }
         }
-        else if(input =="go west" && currentArea != "nBog"){
+        else if(input =="go west" && currentArea == "wBog"){
             $("<p> >> "+input+"</p>").insertBefore("#placeholder")
             $("<p>You cannot travel west from here.</p>").insertBefore("#placeholder")
-        //////////////////////////////////////////////////////////////////////////////////////////////
-        }else if(hitpoints <= 0){
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        }else if(player.hitpoints <= 0){
             $("<p>YOU DEAD</p>").insertBefore("#placeholder")
         }
         else{
